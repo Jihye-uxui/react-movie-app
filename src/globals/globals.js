9f3f3fedc2a75af2dde2e8750cac7c13a4896ea1
Jihye Choe <choijihye37@gmail.com>
2021-04-28 18:27:45 -0700

    Day 2 commit

--- src/globals/globals.js
+++ src/globals/globals.js
@@ -1,1 +1,8 @@
 export const appTitle = 'Kine Movie App';
+export const apiKey = '09f8ace8a3be8200da0914f4f22f3f8c';
+export const posterPath = 'https://image.tmdb.org/t/p/w300/';
+
+export const endPointPopular= 'https://api.themoviedb.org/3/discover/movie?api_key=09f8ace8a3be8200da0914f4f22f3f8c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
+
+// vote average desc
+export const endPointTopRated = 'https://api.themoviedb.org/3/discover/movie?api_key=09f8ace8a3be8200da0914f4f22f3f8c&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,61 +1,64 @@
 // Page - Home
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { appTitle } from '../globals/globals';
-
-// import movie poster images (manually... for now!)
-import poster01 from '../images/01-wonder-woman.jpg';
-import poster02 from '../images/02-below-zero.jpg';
-import poster03 from '../images/03-the-little-things.jpg';
-import poster04 from '../images/04-outside-the-war.jpg';
-import poster05 from '../images/05-black-water.jpg';
-import poster06 from '../images/06-Vanguard.jpg';
-import poster07 from '../images/07-breach.jpg';
-import poster08 from '../images/08-soul.jpg';
-import poster09 from '../images/09-skylines.jpg';
-import poster10 from '../images/10-tenet.jpg';
-import poster11 from '../images/11-finding-ohana.jpg';
-import poster12 from '../images/12-space-sweepers.jpg';
+import { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { appTitle, endPointPopular, posterPath, apiKey } from '../globals/globals';
 
 // SCSS style file
-import '../scss/components/_gallery.scss';
+import '../scss/components/_movie-list.scss';
 
-
+// Fetch movie lists
 const PageHome = () => {
+    const [movies, setMovies] = useState(null);
 
     useEffect(() => {
 		document.title = `${appTitle} - Home`;
-	}, []);
+
+        // Asyncronous requests
+        const fetchMovies = async() => {
+            const res = await fetch(endPointPopular);
+            let data = await res.json();
+
+            console.log(data.results);
+            setMovies(data.results);
+        }
+        fetchMovies();
+    
+    }, []);
+
+
+    function blur(e){
+        e.target.blur();
+    }
 
     return (
         <main>
             <section>
+            <nav className="main-nav" onClick={blur}>
+                <ul>
+                    <li><NavLink to="/" exact>Popular</NavLink></li>
+                    <li><NavLink to="/favourites">Top Rated</NavLink></li>
+                    <li><NavLink to="/about">Upcoming</NavLink></li>
+                    <li><NavLink to="/about">Now Playing</NavLink></li>
+
+                </ul>
+            </nav>
+
                 <h2>Poplular Now</h2>
                 <div className="gallery">
 
-                    <Link to="movie1" className="movie-poster"><img src={poster01} alt="Movie 1"/></Link>
+                    {movies !== null &&
+                    <div name="movie-list">
+                        {movies.map(movie => 
+                        <div key={movie.id}>
+                            <img src={posterPath + movie.poster_path} alt={movie.title}/>
+                            <p>{movie.vote_average}</p>
+                            <p>{movie.release_date}</p>
+                            <h3>{movie.title}</h3>
+                            <p>{movie.overview}</p>
+                        </div>)}
+                    </div>}
 
-                    <Link to="movie2" className="movie-poster"><img src={poster02} alt="Movie 2"/></Link>
-
-                    <Link to="movie3" className="movie-poster"><img src={poster03} alt="Movie 3"/></Link>
-
-                    <Link to="movie4" className="movie-poster"><img src={poster04} alt="Movie 4"/></Link>
-
-                    <Link to="movie5" className="movie-poster"><img src={poster05} alt="Movie 5"/></Link>
-
-                    <Link to="movie6" className="movie-poster"><img src={poster06} alt="Movie 6"/></Link>
-
-                    <Link to="movie7" className="movie-poster"><img src={poster07} alt="Movie 7"/></Link>
-
-                    <Link to="movie8" className="movie-poster"><img src={poster08} alt="Movie 8"/></Link>
-
-                    <Link to="movie9" className="movie-poster"><img src={poster09} alt="Movie 9"/></Link>
-
-                    <Link to="movie10" className="movie-poster"><img src={poster10} alt="Movie 10"/></Link>
-
-                    <Link to="movie11" className="movie-poster"><img src={poster11} alt="Movie 11"/></Link>
-
-                    <Link to="movie12" className="movie-poster"><img src={poster12} alt="Movie 12"/></Link>
+                    {/* <Link to="movie1" className="movie-poster"><img src={poster01} alt="Movie 1"/></Link>*/}
                 </div>
             </section>
         </main>

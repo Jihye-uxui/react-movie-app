a142e9e5a40d5c5967f247416a03e342229451f1
Jihye Choe <choijihye37@gmail.com>
2021-05-03 23:47:26 -0700

    EDIT SCSS files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,25 @@
 // Footer
 
 import { getYear } from '../utilities/utilities';
+import '../scss/components/_footer.scss';
+
+// import logo image
+import logo from '../images/kinemovie-logo.png';
 
 const Footer = () => (
-    <footer>
-        <p>&copy; {getYear()} Jihye Choe.</p>
-    </footer>
+    <div className="wrapper">
+        <footer>
+            <div className="footer-contents">
+                <div className="footer-logo">
+                    <img src={logo} alt="Kine Movie Logo"/>
+                </div>
+                <div>
+                    <p>&copy; {getYear()} TMDb All Rights Reserved.</p>
+                    <p>Created by Jihye Choe For Educational Purpose Only</p>
+                </div>
+            </div>
+        </footer>
+    </div>
 );
 
 export default Footer;
